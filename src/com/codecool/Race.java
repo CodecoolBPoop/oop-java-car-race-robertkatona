897631538d476ef06bf9f576d3c13788528cfeac
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-07-21 15:26:14 +0200

    Vehicle class created

--- src/com/codecool/Race.java
+++ src/com/codecool/Race.java
@@ -3,6 +3,13 @@ package com.codecool;
 public class Race {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Vehicle vehicleOne = new Vehicle("porsche", 500.0, 200.0);
+
+
+        System.out.println(vehicleOne.getName());
+        System.out.println(vehicleOne.getDistanceTraveled());
+        System.out.println(vehicleOne.getMoveForAnHour());
+
     }
 }
